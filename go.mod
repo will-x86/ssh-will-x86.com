0bdc96f19a588cab3d6c57269c6f3e9ebcb85b07
will-x86 <will@kimbell.uk>
2025-04-01 12:26:18 +0100

    Exploring more

--- go.mod
+++ go.mod
@@ -6,6 +6,7 @@ require (
 	github.com/charmbracelet/log v0.4.1
 	github.com/charmbracelet/ssh v0.0.0-20250213143314-8712ec3ff3ef
 	github.com/charmbracelet/wish v1.4.7
+	golang.org/x/crypto v0.36.0
 )
 
 require (
@@ -33,7 +34,6 @@ require (
 	github.com/muesli/termenv v0.16.0 // indirect
 	github.com/rivo/uniseg v0.4.7 // indirect
 	github.com/xo/terminfo v0.0.0-20220910002029-abceb7e1c41e // indirect
-	golang.org/x/crypto v0.36.0 // indirect
 	golang.org/x/exp v0.0.0-20240719175910-8a7402abbf56 // indirect
 	golang.org/x/sync v0.12.0 // indirect
 	golang.org/x/sys v0.31.0 // indirect
--- main.go
+++ main.go
@@ -2,7 +2,9 @@ package main
 
 import (
 	"context"
+	_ "embed"
 	"errors"
+	"fmt"
 	"net"
 	"os"
 	"os/signal"
@@ -12,7 +14,9 @@ import (
 	"github.com/charmbracelet/log"
 	"github.com/charmbracelet/ssh"
 	"github.com/charmbracelet/wish"
+	"github.com/charmbracelet/wish/elapsed"
 	"github.com/charmbracelet/wish/logging"
+	gossh "golang.org/x/crypto/ssh"
 )
 
 const (
@@ -20,28 +24,41 @@ const (
 	port = "23234"
 )
 
+//go:embed banner.txt
+var banner string
+
 func main() {
 	srv, err := wish.NewServer(
-		// The address the server will listen to.
+
 		wish.WithAddress(net.JoinHostPort(host, port)),
 
-		// The SSH server need its own keys, this will create a keypair in the
-		// given path if it doesn't exist yet.
-		// By default, it will create an ED25519 key.
 		wish.WithHostKeyPath(".ssh/id_ed25519"),
+		wish.WithBannerHandler(func(ctx ssh.Context) string {
+			return fmt.Sprintf(banner, ctx.User())
+		}),
+		wish.WithKeyboardInteractiveAuth(func(ctx ssh.Context, challenger gossh.KeyboardInteractiveChallenge) bool {
+			log.Info("keyboard interactive challenge")
+			answers, err := challenger(
+				"", `Possible answers are "vim" or "other"`, []string{"Best ide?"}, []bool{true},
+			)
+			if err != nil {
+				log.Error("Error with answers", "error", err)
+				return false
+			}
+			return len(answers) == 1 && answers[0] == "vim"
+		}),
 
-		// Middlewares do something on a ssh.Session, and then call the next
-		// middleware in the stack.
 		wish.WithMiddleware(
 			func(next ssh.Handler) ssh.Handler {
 				return func(sess ssh.Session) {
-					wish.Println(sess, "Hello, world!")
+					wish.Println(sess, fmt.Sprintf("Hello %s", sess.User()))
 					next(sess)
 				}
 			},
 
 			// The last item in the chain is the first to be called.
 			logging.Middleware(),
+			elapsed.Middleware(),
 		),
 	)
 	if err != nil {
@@ -52,7 +69,6 @@ func main() {
 	go func() {
 		log.Info("Starting SSH server", "host", host, "port", port)
 		if err = srv.ListenAndServe(); err != nil && !errors.Is(err, ssh.ErrServerClosed) {
-			// We ignore ErrServerClosed because it is expected.
 			log.Error("Could not start server", "error", err)
 			done <- nil
 		}
